4cd132e0be92d7af2b9b5333ad06c6f2ccf1b125
Galbator <79435064+Galbator@users.noreply.github.com>
2023-02-14 14:33:34 -0600

    Update to CSCI-364 HW2

--- Programming_Projects/CSCI-364_Java/HW2/hw2/hw2/src/Server.java
+++ Programming_Projects/CSCI-364_Java/HW2/hw2/hw2/src/Server.java
@@ -1,54 +1,84 @@
-/**
- * Server.java
- *
- * My message-oriented middleware (MOM) server.
- *
- * @author David Apostal
- */ 
-
+/*
+ * Program Name: Server.java
+ * 
+ * Java Version: JDK 17.0.5.8 Microsoft
+ * 
+ * Author: Mason A. Motschke
+*/
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.lang.Thread;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
 
-public class Server {
-    private static ServerSocket clientServerSocket;
-    //private final ServerSocket adminServerSocket;
-    private static final int MIN_PORT = 1024;
-    private static final int MIN_QUEUE_SIZE = 1;
-    public static void main(String[] args) throws Exception {
-        Server my_s = new Server();
+public class Server extends Thread {
 
-        if (args.length != 2) {
-            my_s.usage();
-            System.exit(1);
-        }
-        System.out.println("That's right");
+    static Scanner keyboard;
+    static ServerSocket serverSocket;
+    static int connections;
 
-        // setup data structures
-        
-        // process command line args
-        int clientPort = Integer.parseInt(args[0]);
-        int maxSize = Integer.parseInt(args[1]);
-        //int adminPort = Integer.parseInt(args[2]);
-
-        // set up server.
-        clientServerSocket = new ServerSocket(clientPort);
-        //adminServerSocket = new ServerSocket(adminPort);
-
-        while (true) {
-            System.out.println("***** waiting for next client****");
-            Socket socket = clientServerSocket.accept();
-            Runnable cr = new ClientRunnable(socket);
-            Thread thread = new Thread(cr);
-            thread.start();
-        }
+    
+    public void run(){
+        System.out.println("\n - Hi I'm " + this.getName());
+        return;
     }
 
-    private void usage() {
-        System.err.println("Usage: java Server <client port> <max queue size>");
-        System.err.println("where");
-        System.err.println("\tclient port >= " + MIN_PORT);
-        System.err.println("\tmax queue size >= " + MIN_QUEUE_SIZE);
+    public static int getOpenSpot (Server threads[]){
+        for (int i = 0; i < threads.length; i++){
+            if (threads[i] == null){
+                return i;
+            }
+            else if (!threads[i].isAlive()){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main (String args[]){
+        
+        // Setup
+        if (args.length < 2){
+            System.out.println(" -- Error: Not Enough Arguments\n\n -- java Server <port_number> <max_queue_size>");
+            System.exit(0);
+        }
+        System.out.println(" --- Setting Up...");
+
+        // Socket
+        try {
+            serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+        } catch (Exception e) {
+            System.out.println("-- Error: " + e);
+            System.exit(0);
+        }
+        int maxConnections = 100, connections = 0;
+        Server threads[] = new Server[maxConnections];
+        System.out.println(" --- Finished Setting Up\n");
+
+        while (true){
+
+            try {
+                System.out.println("\n --- Waiting for Connection...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println(" -- Connecting...");
+                while (connections == maxConnections){
+                    Thread.sleep(1000);
+                }
+                int open = getOpenSpot(threads);
+                threads[open] = new Server();
+                threads[open].start();
+                System.out.println(" -- Got Connection!");
+                connections++;
+            }
+            catch (Exception e){
+                System.out.println("\n --- Error: " + e + ". Exiting Program...");
+                System.exit(0);
+            }
+
+        }
+
     }
 
 }
-
