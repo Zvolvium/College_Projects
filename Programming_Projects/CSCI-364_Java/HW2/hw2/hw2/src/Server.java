13ab2ea82d51f1a27f9c65d13b4d41f07eebcc87
Galbator <79435064+Galbator@users.noreply.github.com>
2023-02-14 20:19:03 -0600

    Finished CSCI-364 HW2

--- Programming_Projects/CSCI-364_Java/HW2/hw2/hw2/src/Server.java
+++ Programming_Projects/CSCI-364_Java/HW2/hw2/hw2/src/Server.java
@@ -7,7 +7,6 @@
 */
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 import java.lang.Thread;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
@@ -15,8 +14,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import hw2.xml.MessageBuilder;
 import hw2.xml.MessageParser;
-
-import java.io.IOException;
 
 public class Server extends Thread {
 
@@ -26,33 +23,98 @@ public class Server extends Thread {
     static ArrayList<String> Queue;
     static int maxQueue;
 
+    static final int MAX_CONNECTIONS = 10;
+    static final int SLEEP_TIME = 1000;
+    static final int MIN_PORT = 1024;
+
     public Server (ArrayList<String> Queue, Socket client, int maxQueue){
-        this.Queue = Queue;
-        this.clientSocket = client;
-        this.maxQueue = maxQueue;
+        Server.Queue = Queue;
+        Server.clientSocket = client;
+        Server.maxQueue = maxQueue;
     }
     
     public void run(){
-        System.out.println("\n - Hi I'm " + this.getName());
         try {
-            //MessageBuilder mb = new MessageBuilder();
-            //MessageParser mp = new MessageParser();
-            PrintWriter writer = new PrintWriter(this.clientSocket.getOutputStream(), true);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            MessageParser mp;
+            PrintWriter writer = new PrintWriter(Server.clientSocket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(Server.clientSocket.getInputStream()));
+            String cmd;
+            String data;
 
-            // while (){
+            mp = new MessageParser(reader.readLine());
+            while (!mp.getCmd().strip().equals("close")){
+                
+                if (mp.getCmd().strip().equals("write")){
+                    MessageBuilder mb = new MessageBuilder();
+                    cmd = mp.getCmd().strip();
+                    data = mp.getData().strip();
 
-            // }
+                    synchronized (Server.Queue){
+                        if (Queue.size() < Server.maxQueue){
+                            Server.Queue.add(data);
+                            data = "ok";
+                        } else {
+                            data = "Error: Queue Full";
+                        }
+                    }
+                    mb.setCmd(cmd);
+                    mb.setData(data);
+                    writer.println(mb.toString());
+                }
+                else if (mp.getCmd().strip().equals("read")){
+                    MessageBuilder mb = new MessageBuilder();
+                    cmd = mp.getCmd().strip();
+
+                    synchronized (Server.Queue){
+                        if (!Server.Queue.isEmpty()){
+                            data = Server.Queue.remove(0);
+                        } else {
+                            data = "Error: Queue Empty";
+                        }
+                    }
+                    mb.setCmd(cmd);
+                    mb.setData(data);
+                    writer.println(mb.toString());
+                }
+                else if (mp.getCmd().strip().equals("size")){
+                    MessageBuilder mb = new MessageBuilder();
+                    cmd = mp.getCmd().strip();
+
+                    synchronized (Server.Queue){
+                        int size = Server.Queue.size();
+                        data = "".format("%d", size);
+                    }
+
+                    mb.setCmd(cmd);
+                    mb.setData(data);
+                    writer.println(mb.toString());
+                } else {
+                    MessageBuilder mb = new MessageBuilder();
+                    cmd = "Error_No_Such_Command";
+                    data = "Error";
+                    mb.setCmd(cmd);
+                    mb.setData(data);
+                    writer.println(mb.toString());
+                }
+                mp = new MessageParser(reader.readLine());
+            }
+
+            MessageBuilder mb = new MessageBuilder();
+            cmd = mp.getCmd().strip();
+            data = "ok";
+            mb.setCmd(cmd);
+            mb.setData(data);
+            writer.println(mb.toString());
             
-            this.clientSocket.close();
+            Server.clientSocket.close();
             return;
         } catch (Exception e){
-
+            System.out.println(" --- Error: " + e);
         }
     }
 
     public static int getOpenSpot (Server threads[]){
-        for (int i = 0; i < threads.length; i++){
+        for (int i = 0; i < MAX_CONNECTIONS; i++){
             if ((threads[i] == null) || (!threads[i].isAlive())){
                 return i;
             }
@@ -60,35 +122,34 @@ public class Server extends Thread {
         return -1;
     }
 
-    public synchronized String queueRead (ArrayList<String> queue){
-        if (!queue.isEmpty()){
-            return queue.remove(0);
-        } else {
-            return "Error: Queue Empty";
+    public static void checkArgs (String args[]){
+        try {
+            int port = Integer.parseInt(args[0]);
+            int maxQueueSize = Integer.parseInt(args[1]);
+            if ((port < MIN_PORT) || (maxQueueSize < 0)){
+                throw new Exception("Args not Valid");
+            }
+        } catch (Exception e){
+            System.out.println(" --- Error: Args not Valid\n\n -- java Server <port_number> <max_queue_size>");
+            System.out.println("    -- port_number > 1024\n    -- max_queue_size > 0");
+            System.exit(0);
         }
-    }
-
-    public synchronized int queueSize (ArrayList<String> queue){
-        return queue.size();
-    }
-
-    public synchronized void queueWrite (ArrayList<String> queue, int maxSize){
-        
+        return;
     }
 
     public static void main (String args[]){
         
         // Setup
         if (args.length < 2){
-            System.out.println(" -- Error: Not Enough Arguments\n\n -- java Server <port_number> <max_queue_size>");
+            System.out.println(" --- Error: Not Enough Arguments\n\n -- java Server <port_number> <max_queue_size>");
             System.exit(0);
         }
         System.out.println(" --- Setting Up...");
-        int maxConnections = 100, connections = 0;
-        Server threads[] = new Server[maxConnections];
+        Server threads[] = new Server[MAX_CONNECTIONS];
 
         // Socket
         try {
+            checkArgs(args);
             Queue = new ArrayList<String>();
             serverSocket = new ServerSocket(Integer.parseInt(args[0]));
             maxQueue = Integer.parseInt(args[1]);
@@ -98,20 +159,24 @@ public class Server extends Thread {
         }
         System.out.println(" --- Finished Setting Up\n");
 
+        int open;
         while (true){
 
             try {
+                open = getOpenSpot(threads);
+                if (open == -1){
+                    System.out.println("\n --- Max Client Connections Made!\n     - Waiting for Client to Close...\n");
+                    while (open == -1){
+                        Thread.sleep(SLEEP_TIME);
+                        open = getOpenSpot(threads);
+                    }
+                }
                 System.out.println("\n --- Waiting for Connection...");
                 clientSocket = serverSocket.accept();
-                System.out.println(" -- Connecting...");
-                while (connections == maxConnections){
-                    Thread.sleep(1000);
-                }
-                int open = getOpenSpot(threads);
+                System.out.println("     -- Connecting...");
                 threads[open] = new Server(Queue, clientSocket, maxQueue);
                 threads[open].start();
-                System.out.println(" -- Got Connection!");
-                connections++;
+                System.out.println("     -- Got Connection!");
             }
             catch (Exception e){
                 System.out.println("\n --- Error: " + e + ". Exiting Program...");
